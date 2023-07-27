cad47620a647ebe4797333c09e2ce1c9f921ad43
Ruben Akande <akanderuben17@gmail.com>
2023-07-27 18:00:41 +0100

    added two button

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-let display = document.getElementById('display')
+const display = document.getElementById('display')
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+
 let getLocation = () => {
-    alert("Hi there")
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition)
+
     } else {
         display.innerHTML ="Geolocation is not available"
     }
@@ -10,4 +13,40 @@ let getLocation = () => {
 
 let showPosition = (position) => {
     display.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude
+    console.log(position)
 }
+
+const url = 'https://script.google.com/macros/s/AKfycbyijU0gnErpMxE7J_wXeQ8gPBgTJmZVkXM06LwEKaY2jSBoH4X2C0B6bR--xTKGFZuj/exec'
+btn1.addEventListener('click',(e) =>{
+            fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'text/plain;charset=utf-8',
+                }
+            }).then(res => res.json())
+                .then(data => {
+                console.log("success:", data);
+                // console.log(data)
+            })
+            .catch(err => {
+                console.log("Error:" + err);
+            });
+        })
+
+btn2.addEventListener('click',(e) =>{
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain;charset=utf-8',
+            },
+        body: JSON.stringify({a: 2}),
+        }).then(res => res.json())
+        .then(data => {
+        console.log("success:", data);
+                // console.log(data)
+        })
+        .catch(err => {
+            console.log("Error:" + err);
+        });
+    })
+
